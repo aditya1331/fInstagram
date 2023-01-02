f4395cce4959a22826cc3fd62dc6ae83bc4d58e4
Aditya Deshpande <54365703+aditya1331@users.noreply.github.com>
2023-01-02 21:56:09 +0530

    Create a GlobalKey Set the key in form to the one that is defined The key ensures that everytime the app is rerendered its state is recorded Adding email and password fields and incorporating a validator function Setting the password field to obscure = true so that no one can see it during typing

--- lib/Pages/Login_page.dart
+++ lib/Pages/Login_page.dart
@@ -7,7 +7,9 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   double? _deviceHeight, _deviceWidth;
+  String? _email, _password;
   GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     _deviceWidth = MediaQuery.of(context).size.width;
@@ -23,6 +25,7 @@ class _LoginPageState extends State<LoginPage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 _titleWidget(),
+                _loginForm(),
                 _loginButton(),
               ],
             ),
@@ -39,28 +42,60 @@ class _LoginPageState extends State<LoginPage> {
           color: Colors.black, fontSize: 25, fontWeight: FontWeight.w600),
     );
   }
-  Widget _loginForm()
-  {
+
+  Widget _loginForm() {
     return Container(
-      height: _deviceHeight!*0.2,
+      height: _deviceHeight! * 0.2,
       child: Form(
-        key: _loginFormKey, //It allows to control form and holds the data required
+        key: _loginFormKey,
+        //It allows to control form and holds the data required
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-
+            _emailTextField(),
+            _passwordTextField(),
           ],
         ),
       ),
     );
   }
-  Widget _emailTextField(){
+
+  Widget _emailTextField() {
     return TextFormField(
+      decoration: const InputDecoration(hintText: "Email..."),
+      onSaved: (_value) {
+        setState(() {
+          _email = _value;
+        });
+      },
+      validator: (_value) {
+        bool _results = _value!.contains(RegExp(
+            r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+        );
+        _results ? null:"Please enter a valid email";
+      }, //validates the input
 
     );
   }
+
+  Widget _passwordTextField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: const InputDecoration(hintText: "Password..."),
+      onSaved: (_value) {
+        setState(() {
+          _password = _value;
+        });
+      },
+      validator: (_value) =>
+        _value!.length> 6 ? null:"Please enter a password greater than charcters",
+       //validates the input
+
+    );
+  }
+
   Widget _loginButton() {
     return MaterialButton(
       onPressed: () {},
