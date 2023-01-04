36b5c81022bd1e8090977cdac61feb257f53ec6f
Aditya Deshpande <54365703+aditya1331@users.noreply.github.com>
2023-01-05 01:37:22 +0530

    Created a home page Added link to navigate to Made homepage as default Set the navigation bar to naviagate between feed and profile page

---
+++ lib/Pages/feed_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class feedPage extends StatefulWidget {
+
+
+  @override
+  State<feedPage> createState() => _feedPageState();
+}
+
+class _feedPageState extends State<feedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple,
+    );
+  }
+}
--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,8 +1,8 @@
+import 'package:finstagram/Pages/feed_page.dart';
+import 'package:finstagram/Pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -10,13 +10,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int? _currentPage = 0;
 
-  List<Widget> _pages = [
-    Container(
-      color: Colors.green,
-    ),
-    Container(
-      color: Colors.blue,
-    )
+  final List<Widget> _pages = [
+    feedPage(),
+    ProfilePage(),
   ];
 
   @override
@@ -25,13 +21,16 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: bottomBar(),
       body: _pages[_currentPage!],
       appBar: AppBar(
-        title: const Text("FInstagram",),
+        title: const Text(
+          "FInstagram",
+        ),
         actions: [
           GestureDetector(
             onTap: () {},
             child: const Icon(Icons.add_a_photo),
           ),
-          Padding(padding: const EdgeInsets.only(
+          Padding(
+              padding: const EdgeInsets.only(
             left: 8.0,
             right: 8.0,
           )),
@@ -53,14 +52,11 @@ class _HomePageState extends State<HomePage> {
           });
         },
         items: const [
-          BottomNavigationBarItem(
-              label: "Feed",
-              icon: Icon(Icons.feed)),
+          BottomNavigationBarItem(label: "Feed", icon: Icon(Icons.feed)),
           BottomNavigationBarItem(
             label: "Profile",
             icon: Icon(Icons.account_box),
           )
-
         ]);
   }
 }
